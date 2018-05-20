babc05a9baad024d4063561f5630d90bee0a5665
riaanggraini99 <ria.anggraini@students.amikom.ac.id>
2018-05-20 11:21:41 +0700

    deletin duplicates directory

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express           = require('express');
 var path              = require('path');
 var createError       = require('http-errors');
 var express           = require('express');
-var path              = require('path');
+
 var cookieParser      = require('cookie-parser');
 var logger            = require('morgan');
 var bodyParser        = require('body-parser');
